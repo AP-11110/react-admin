4fedc992a9ba8409b55ab5332d6e3adc9fd252fd
Art Pau <arturas.study@gmail.com>
2022-12-11 14:59:01 +0100

    monthly, breakdown & admin pages

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5001' }),
     reducerPath: "adminApi",
-    tagTypes: ["User", "Products", "Customers", "Transactions", "Geography", "Sales"],
+    tagTypes: ["User", "Products", "Customers", "Transactions", "Geography", "Sales", "Admins"],
     endpoints: (build) => ({
         getUser: build.query({
             query: (id) => `general/user/${id}`,
@@ -32,10 +32,14 @@ export const api = createApi({
         getSales: build.query({
             query: () => "sales/sales", 
             providesTags: ["Sales"]
+        }),
+        getAdmins: build.query({
+            query: () => "management/admins",
+            providesTags: ["Admins"]
         })
     })
 })
 
 export const {
-    useGetUserQuery, useGetProductsQuery, useGetCustomersQuery, useGetTransactionsQuery, useGetGeographyQuery, useGetSalesQuery
+    useGetUserQuery, useGetProductsQuery, useGetCustomersQuery, useGetTransactionsQuery, useGetGeographyQuery, useGetSalesQuery, useGetAdminsQuery
 } = api;
